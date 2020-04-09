460be342889b4954c4da70184d96560c22580554
PelleK <pellekoster@matgras.nl>
2020-04-09 20:36:35 +0200

    update docs build config

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = "Crown's Dilemma"
-copyright = '2020, Pelle Koster'
-author = 'Pelle Koster'
+copyright = "2020, Pelle Koster"
+author = "Pelle Koster"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,17 +28,17 @@ author = 'Pelle Koster'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'matplotlib.sphinxext.mathmpl',
-             'matplotlib.sphinxext.plot_directive',
+    "matplotlib.sphinxext.mathmpl",
+    "matplotlib.sphinxext.plot_directive",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "venv"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,9 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = "nature"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
