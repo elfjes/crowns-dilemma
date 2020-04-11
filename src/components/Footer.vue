f844607c32c8542829186ac6fff1736a04772d5b
PelleK <pellekoster@matgras.nl>
2020-04-11 19:19:34 +0200

    rm trailing separator

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
         <strong>Crown's Dilemma</strong> (v{{ version }}) by Pelle Koster. The
         <a href="https://github.com/elfjes/crowns-dilemma">source code</a> is licensed
         <a href="https://opensource.org/licenses/mit-license.php">MIT</a>. &nbsp;&bull;
-        <a href="https://crowns-dilemma.readthedocs.io/">Documentation</a> &nbsp;&bull;
+        <a href="https://crowns-dilemma.readthedocs.io/">Documentation</a>
       </p>
     </div>
   </footer>
